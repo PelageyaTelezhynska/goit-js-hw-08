feb0452e62558f25b7978991f5d1e6a7d0746c52
PelageyaTelezhynska <p.yu.telezhynska@gmail.com>
2022-10-05 15:22:57 +0300

    tasks 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Vimeo from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+const TIME_DATA = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+    
+function onTimeUpdate(data) {
+    localStorage.setItem(TIME_DATA, JSON.stringify(data));
+    }
+
+function pageUpdate() {
+       try {
+           const currentTime= localStorage.getItem(TIME_DATA);
+           const savedCurrentTime = JSON.parse(currentTime);
+           player.setCurrentTime(savedCurrentTime.seconds)
+       } catch (error) {
+        console.log(error);
+       }
+}
+   
+pageUpdate()
+
+    
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,12 +2,51 @@ const throttle = require('lodash.throttle');
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  inputEmail: document.querySelector('[name="email"]'),
+  submit: document.querySelector('button'),
 };
 
-const inputData = 'feedback-form-state';
-refs.form.addEventListener('input', throttle(onInput), 500);
+const INFO = 'feedback-form-state';
+const formData = {};
+const inputEmail = refs.form.elements.email;
+const inputMessage = refs.form.elements.message;
+
+console.log(inputEmail);
+refs.form.addEventListener('input', throttle(onInput, 500));
 
 function onInput(e) {
-    // localStorage.setItem(inputData, JSON.stringify({ inputEmail: refs.email.value, })
+  if (inputEmail === e.target) {
+    formData.email = e.target.value;
+  }
+
+  if (inputMessage === e.target) {
+    formData.message = e.target.value;
+  }
+  
+  localStorage.setItem(INFO, JSON.stringify(formData));
 }
+
+refs.submit.addEventListener('click', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (inputEmail.value === '' || inputMessage.value === '') {
+    return alert('Будь-ласка заповніть всі поля форми!')
+  }
+  refs.form.reset();
+    localStorage.removeItem(INFO);
+  console.log(formData);
+  
+}
+
+function updateInput() {
+  try {
+    const localData = localStorage.getItem(INFO);
+    const data = JSON.parse(localData);
+      inputEmail.value = data.email;
+      inputMessage.value = data.message;
+  } catch (error) {
+    console.log(error);
+  } 
+}
+
+updateInput()
