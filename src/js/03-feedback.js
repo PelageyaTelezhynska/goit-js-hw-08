ca29f5e91777fe962fc6804dccb1dbb63a432d7a
PelageyaTelezhynska <p.yu.telezhynska@gmail.com>
2022-10-05 08:06:37 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+const throttle = require('lodash.throttle');
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('[name="email"]'),
+};
+
+const inputData = 'feedback-form-state';
+refs.form.addEventListener('input', throttle(onInput), 500);
+
+function onInput(e) {
+    // localStorage.setItem(inputData, JSON.stringify({ inputEmail: refs.email.value, })
+}
