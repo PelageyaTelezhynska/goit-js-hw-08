7d2a0af65e8a32ecff16a033c8506389e4bff6dd
PelageyaTelezhynska <p.yu.telezhynska@gmail.com>
2022-10-06 20:02:07 +0300

    correct tasks 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,26 +3,24 @@ const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
-const TIME_DATA = "videoplayer-current-time";
+const TIME_DATA = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
-    
+
 function onTimeUpdate(data) {
-    localStorage.setItem(TIME_DATA, JSON.stringify(data));
-    }
+  localStorage.setItem(TIME_DATA, JSON.stringify(data));
+}
 
 function pageUpdate() {
-       try {
-           const currentTime= localStorage.getItem(TIME_DATA);
-           const savedCurrentTime = JSON.parse(currentTime);
-           player.setCurrentTime(savedCurrentTime.seconds)
-       } catch (error) {
-        console.log(error);
-       }
+  try {
+    if (localStorage.length !== 0) {
+      const currentTime = localStorage.getItem(TIME_DATA);
+      const savedCurrentTime = JSON.parse(currentTime);
+      player.setCurrentTime(savedCurrentTime.seconds);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
-   
-pageUpdate()
 
-    
-
-
+pageUpdate();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,22 +6,16 @@ const refs = {
 };
 
 const INFO = 'feedback-form-state';
-const formData = {};
 const inputEmail = refs.form.elements.email;
 const inputMessage = refs.form.elements.message;
 
-console.log(inputEmail);
 refs.form.addEventListener('input', throttle(onInput, 500));
 
 function onInput(e) {
-  if (inputEmail === e.target) {
-    formData.email = e.target.value;
-  }
-
-  if (inputMessage === e.target) {
-    formData.message = e.target.value;
-  }
-  
+  const formData = {
+    email: inputEmail.value,
+    message: inputMessage.value,
+  };
   localStorage.setItem(INFO, JSON.stringify(formData));
 }
 
@@ -30,23 +24,24 @@ refs.submit.addEventListener('click', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
   if (inputEmail.value === '' || inputMessage.value === '') {
-    return alert('Будь-ласка заповніть всі поля форми!')
+    return alert('Будь-ласка заповніть всі поля форми!');
   }
   refs.form.reset();
-    localStorage.removeItem(INFO);
-  console.log(formData);
-  
+  localStorage.removeItem(INFO);
 }
 
 function updateInput() {
   try {
     const localData = localStorage.getItem(INFO);
     const data = JSON.parse(localData);
+    console.log(data);
+    if (data) {
       inputEmail.value = data.email;
       inputMessage.value = data.message;
+    }
   } catch (error) {
     console.log(error);
-  } 
+  }
 }
 
-updateInput()
+updateInput();
